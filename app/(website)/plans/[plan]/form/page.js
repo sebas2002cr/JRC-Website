d5681d787bc451391982b490ee2f8f38090eaa0f
Sebastian Obando Paniagua <sebas200205@gmail.com>
2024-10-04 14:22:25 -0600

    useCallback

--- app/(website)/plans/[plan]/form/page.js
+++ app/(website)/plans/[plan]/form/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useRouter, useParams } from "next/navigation";
 import Link from "next/link";
